6c37d22d2dfb1f7eef64d29b076e2d96baaba2f5
Espen Lorentzen <esplor@gmail.com>
2021-01-04 21:37:04 +0100

    Update ipxe script

--- update-ipxe.sh
+++ update-ipxe.sh
@@ -5,12 +5,12 @@ echo updating submodules
 #Did not work: git submodule update --init --recursive
 
 git submodule update --recursive --remote
-cd ipxe.org/src/
+cd ipxe/src/
 
 echo Make clean \(very\)...
 make veryclean
 
-echo making undionly and iso with embedded script.
+echo Making undionly, iso and x64 efi with embedded script.
 make bin/undionly.kpxe EMBED=../../esplor.github.io.boot.ipxe
 make bin/ipxe.iso EMBED=../../esplor.github.io.boot.ipxe
 make bin-x86_64-efi/ipxe.efi EMBED=../../esplor.github.io.boot.ipxe
@@ -21,7 +21,8 @@ mv bin/undionly.kpxe ../../undionly.kpxe
 echo Moving ipxe.iso to root dir
 mv bin/ipxe.iso ../../ipxe.iso
 
-echo moving 64 bit ipxe.efi to ipxe64.efi root dir
+echo Moving 64 bit ipxe.efi to ipxe64.efi root dir
 mv bin-x86_64-efi/ipxe.efi ../../ipxe64.efi
 
+# Do we need this
 cd ../..
