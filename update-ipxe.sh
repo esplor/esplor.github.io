073997b60d0f40b573a5dbb30bc44578a7f99154
Espen Lorentzen <esplor@gmail.com>
2021-01-11 00:15:05 +0100

    update-ipxe.sh - add i386 efi, use embedfile

--- update-ipxe.sh
+++ update-ipxe.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env sh
+
+# Clear the screen
 clear
+
 echo updating submodules
-
-#Did not work: git submodule update --init --recursive
-
 git submodule update --recursive --remote
+
+# Set relative location of the embedfile.
+EMBEDFILE=../../esplor.github.io.boot.ipxe
+
 cd ipxe/src/
 
 echo Make clean \(very\)...
 make veryclean
 
 echo Making undionly, iso and x64 efi with embedded script.
-make bin/undionly.kpxe EMBED=../../esplor.github.io.boot.ipxe
-make bin/ipxe.iso EMBED=../../esplor.github.io.boot.ipxe
-make bin-x86_64-efi/ipxe.efi EMBED=../../esplor.github.io.boot.ipxe
+make bin/undionly.kpxe 		EMBED=$EMBEDFILE
+make bin/ipxe.iso 		EMBED=$EMBEDFILE
+make bin-i386-efi/ipxe.efi 	EMBED=$EMBEDFILE
+make bin-x86_64-efi/ipxe.efi 	EMBED=$EMBEDFILE
 
 echo Moving undionly.kpxe to root dir
 mv bin/undionly.kpxe ../../undionly.kpxe
@@ -21,7 +26,10 @@ mv bin/undionly.kpxe ../../undionly.kpxe
 echo Moving ipxe.iso to root dir
 mv bin/ipxe.iso ../../ipxe.iso
 
-echo Moving 64 bit ipxe.efi to ipxe64.efi root dir
+echo Moving 32 bit ipxe32.efi to ipxe64.efi root dir
+mv bin-i386-efi/ipxe.efi ../../ipxe32.efi
+
+echo Moving 64 bit ipxe64.efi to ipxe64.efi root dir
 mv bin-x86_64-efi/ipxe.efi ../../ipxe64.efi
 
 # Do we need this
