96fd8736f090d8b54953f887db8ecfc63073035d
Espen Lorentzen <esplor@gmail.com>
2021-08-19 11:59:39 +0200

    Speed up build with nproc

--- update-ipxe.sh
+++ update-ipxe.sh
@@ -16,10 +16,10 @@ echo Make clean \(very\)...
 make veryclean
 
 echo Making undionly, iso and i386 / x86_64 efi with embedded script.
-make bin/undionly.kpxe 		EMBED=$EMBEDFILE
-make bin/ipxe.iso 		EMBED=$EMBEDFILE
-make bin-i386-efi/ipxe.efi 	EMBED=$EMBEDFILE
-make bin-x86_64-efi/ipxe.efi 	EMBED=$EMBEDFILE
+make -j $(nproc) bin/undionly.kpxe 		EMBED=$EMBEDFILE
+make -j $(nproc) bin/ipxe.iso 		EMBED=$EMBEDFILE
+make -j $(nproc) bin-i386-efi/ipxe.efi 	EMBED=$EMBEDFILE
+make -j $(nproc) bin-x86_64-efi/ipxe.efi 	EMBED=$EMBEDFILE
 
 echo Moving undionly.kpxe to root dir
 mv bin/undionly.kpxe ../../undionly.kpxe
